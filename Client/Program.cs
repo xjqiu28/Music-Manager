ce683b3f8754fa93553519dc28541ee604f890d5
xjqiu28 <xjqiu28@gmail.com>
2024-11-07 21:21:16 -0500

    removed unused lines/codes

--- Client/Program.cs
+++ Client/Program.cs
@@ -5,7 +5,6 @@ using music_manager_starter.Client;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddScoped<SearchService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
